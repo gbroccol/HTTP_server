c205b495f214b016528b181e3aa8c5e2331369f5
Shanon Snowbird <ssnowbir@oa-e5.msk.21-school.ru>
2021-05-11 19:44:18 +0300

    need to do * for location

--- sources/main.cpp
+++ sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pvivian <pvivian@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/04 18:27:49 by pvivian           #+#    #+#             */
-/*   Updated: 2021/05/10 17:13:03 by pvivian          ###   ########.fr       */
+/*   Updated: 2021/05/11 18:47:25 by pvivian          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,46 @@
 # include "Config.hpp"
 # include "ParseRequest.hpp"
 
+int isLocation(std::vector<location *> locations, std::string path)
+{
+	int theBestLocation = -1;
+	for(size_t i = 0; i < locations.size(); i++)
+	{
+		if(locations[i]->path == path)
+		{
+			theBestLocation = i;
+			std::cout << theBestLocation<<std::endl;
+			return i;
+		}
+		else
+		{
+			for(size_t j = 0; j < path.length(); j++)
+				if(path[j] == locations[i]->path[j])
+					if(j == locations[i]->path.length() - 1)
+						if(((path[j] == path[path.length() - 1]) || path[j + 1] == '/') || j == 0)
+						{
+
+							if(theBestLocation == -1 ||(theBestLocation >= 0 && locations[i]->path.length() > locations[theBestLocation]->path.length()))
+								theBestLocation = i;
+						}
+		}
+	}
+	std::cout << theBestLocation<<std::endl;
+	return theBestLocation;
+}
+
 int main(int argc,  char **argv)
 {
 	std::vector<Server *> servers;
 	Server * server;
 	Config config;
 	configServer *confServer;
+	// char *configFile;
 
 	try 
 	{
+		// если аргументов нет (argc == 1), configFile = default path
+		// В ином случае configFile = argv[1]
 		if(argc != 2)
 		{
 			std::cout << "Incorrect argc"<<std::endl;
@@ -35,6 +66,7 @@ int main(int argc,  char **argv)
 		for (size_t i = 0; i < config.getSize(); i++)
 		{
 			confServer = config.getconfigServer((int)i);
+			isLocation(confServer->locations, "/abc");
 			server = new Server;
 			server->init(*confServer);
 			servers.push_back(server);
